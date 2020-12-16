17310700543461117d219785ed587a785a196009
viniciusanchieta <vinicius.anchieta@outlook.com>
2020-12-16 20:07:55 -0300

    Added first post of blog

--- next-site/pages/index.tsx
+++ next-site/pages/index.tsx
@@ -1,8 +1,13 @@
-import Head from 'next/head'
+import { InferGetStaticPropsType } from 'next';
+import Head from 'next/head';
+import Link from 'next/link';
+import { getPostList } from '@shared/util';
 import Header from '@components/header';
 import Navigation from '@components/navigation';
 
-export default function Home() {
+type PostList = string[];
+
+function Home({ posts }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <>
       <Head>
@@ -12,7 +17,31 @@ export default function Home() {
       <main>
         <Navigation />
         <Header />
+        {posts.length > 0 && (
+          <ul>
+            {posts.map((slug) => (
+              <li key={slug}>
+                <Link href={`post/${slug}`}>
+                <a>
+                  {slug.replace(/-/g, ' ')}
+                </a>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        )}
       </main>
     </>
   )
 }
+
+export const getStaticProps = async () => {
+  const posts:PostList = getPostList();
+
+  return {
+    props: {
+      posts
+    }
+  }
+}
+export default Home;
---
+++ next-site/pages/post/hello-there.tsx
@@ -0,0 +1,10 @@
+import Navigation from '@components/navigation';
+
+const Hello = () => (
+    <>
+     <Navigation />
+        <h1>Hello There!</h1>
+    </>
+)
+
+export default Hello;
