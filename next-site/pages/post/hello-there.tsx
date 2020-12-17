cfd0903bfc6d828cfa083ed1049567b4130aace8
viniciusanchieta <vinicius.anchieta@outlook.com>
2020-12-16 21:24:20 -0300

    Added Head in page of post for SEO

--- next-site/pages/post/hello-there.tsx
+++ next-site/pages/post/hello-there.tsx
@@ -1,15 +1,22 @@
+import Head from 'next/head';
 import Image from 'next/image';
 import Navigation from '@components/navigation';
 
 const Hello = () => (
-    <article>
-     <Navigation />
-        <h1>Hello There!</h1>
-        <Image src="/posts/hello-there/img.jpeg" 
-        width={600}
-        height={340}
-        />
-    </article>
+    <>
+        <Head>
+            <title>Hello There :: My Blog</title>
+            <meta property="og:title" content="Hello There :: My Blog"/>
+        </Head>
+        <article>
+            <Navigation />
+            <h1>Hello There!</h1>
+            <Image src="/posts/hello-there/img.jpeg"
+                width={600}
+                height={340}
+            />
+        </article>
+    </>
 )
 
 export default Hello;
