d2f1f351a7259371183a65c04982a3ec2c03b3eb
viniciusanchieta <vinicius.anchieta@outlook.com>
2020-12-16 21:20:05 -0300

    Added new component of next 10 the Image

--- next-site/pages/post/hello-there.tsx
+++ next-site/pages/post/hello-there.tsx
@@ -1,10 +1,15 @@
+import Image from 'next/image';
 import Navigation from '@components/navigation';
 
 const Hello = () => (
-    <>
+    <article>
      <Navigation />
         <h1>Hello There!</h1>
-    </>
+        <Image src="/posts/hello-there/img.jpeg" 
+        width={600}
+        height={340}
+        />
+    </article>
 )
 
 export default Hello;
